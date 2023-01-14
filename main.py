f7ddf21eddc9e0329e22db9d886d686a75f0b1f8
Estukoo <timothee.deraynal@gmail.com>
2023-01-15 00:13:20 +0100

    MAMAAAAAA

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ pygame.init()
 # Création de la fenêtre de jeu
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-TITLE = "MAMAMAAAAA"
+TITLE = "MAMAAAAAA"
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(TITLE)
